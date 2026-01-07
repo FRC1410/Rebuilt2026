2ddfe93a7be8756aaf493bddc01c34c72236b71f
Calstar9000 <Calstar9000+github@gmail.com>
2026-01-07 15:44:49 -0700

    tuning add

--- robot/src/main/java/org/frc1410/rebuilt2026/util/Tuning.java
+++ robot/src/main/java/org/frc1410/rebuilt2026/util/Tuning.java
@@ -2,4 +2,26 @@ package org.frc1410.rebuilt2026.util;
 
 public final class Tuning {
 	private Tuning() {}
+    // Drivetrain
+    public static final double SWERVE_DRIVE_P = 0.401;
+    public static final double SWERVE_DRIVE_I = 0;
+    public static final double SWERVE_DRIVE_D = 0.000;
+
+//    public static final double SWERVE_DRIVE_P = 0;
+//    public static final double SWERVE_DRIVE_I = 0;
+//    public static final double SWERVE_DRIVE_D = 0;
+
+    public static final double SWERVE_STEER_P = 4.7; //4.7
+    public static final double SWERVE_STEER_I = 0.0;
+    public static final double SWERVE_STEER_D = 0.0;
+
+//    public static final double SWERVE_STEER_P = 0;
+//    public static final double SWERVE_STEER_I = 0;
+//    public static final double SWERVE_STEER_D = 0;
+
+    public static final double DRIVE_KS = 0.36498;
+    public static final double DRIVE_KV = 0.11769;
+
+    public static double DRIVE_MULTIPLIER = 1.0;
+
 }
