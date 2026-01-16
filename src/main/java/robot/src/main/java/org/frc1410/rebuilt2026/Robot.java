df0cb75df885d58b9e184c041b5b71e9c2486151
Calstar9000 <Calstar9000+github@gmail.com>
2026-01-15 17:16:10 -0700

    Storage disable

--- src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Robot.java
+++ src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Robot.java
@@ -7,9 +7,7 @@ import framework.src.main.java.org.frc1410.framework.control.Controller;
 import framework.src.main.java.org.frc1410.framework.scheduler.task.TaskPersistence;
 import framework.src.main.java.org.frc1410.framework.scheduler.task.lock.LockPriority;
 import robot.src.main.java.org.frc1410.rebuilt2026.commands.DriveLooped;
-import robot.src.main.java.org.frc1410.rebuilt2026.commands.StorageToggleCommand;
 import robot.src.main.java.org.frc1410.rebuilt2026.subsystems.Drivetrain;
-import robot.src.main.java.org.frc1410.rebuilt2026.subsystems.Storage;
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.DRIVER_CONTROLLER;
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.OPERATOR_CONTROLLER;
 
@@ -19,11 +17,11 @@ public final class Robot extends PhaseDrivenRobot {
 	private final Controller driverController = new Controller(this.scheduler, DRIVER_CONTROLLER, 0.1);
 	private final Controller operatorController = new Controller(this.scheduler, OPERATOR_CONTROLLER,  0.1);
 	private final Drivetrain drivetrain = subsystems.track(new Drivetrain(this.subsystems));
-	private final Storage storage = new Storage();
+	// private final Storage storage = new Storage();
 
-	private final StorageToggleCommand intake = new StorageToggleCommand(storage, Storage.StorageStates.INTAKE);
-	private final StorageToggleCommand neutral = new StorageToggleCommand(storage, Storage.StorageStates.NEUTRAL);
-	private final StorageToggleCommand outtake = new StorageToggleCommand(storage, Storage.StorageStates.OUTTAKE);
+	// private final StorageToggleCommand intake = new StorageToggleCommand(storage, Storage.StorageStates.INTAKE);
+	// private final StorageToggleCommand neutral = new StorageToggleCommand(storage, Storage.StorageStates.NEUTRAL);
+	// private final StorageToggleCommand outtake = new StorageToggleCommand(storage, Storage.StorageStates.OUTTAKE);
 
 	@Override
 	public void autonomousSequence() {
@@ -42,9 +40,9 @@ public final class Robot extends PhaseDrivenRobot {
 			TaskPersistence.GAMEPLAY, 
 			LockPriority.HIGH
 		);
-		this.operatorController.A.whenPressed(intake, TaskPersistence.GAMEPLAY);
-		this.operatorController.B.whenPressed(neutral, TaskPersistence.GAMEPLAY);
-		this.operatorController.X.whenPressed(outtake, TaskPersistence.GAMEPLAY);
+		// this.operatorController.A.whenPressed(intake, TaskPersistence.GAMEPLAY);
+		// this.operatorController.B.whenPressed(neutral, TaskPersistence.GAMEPLAY);
+		// this.operatorController.X.whenPressed(outtake, TaskPersistence.GAMEPLAY);
 	}
 
 
