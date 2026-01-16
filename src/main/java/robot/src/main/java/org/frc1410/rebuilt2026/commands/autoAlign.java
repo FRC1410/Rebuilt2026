0a80bbbac2151c371695a143c94c303f8bb1a54c
Greek Question Mark <galaxyisfake@gmail.com>
2026-01-16 16:52:05 -0700

    my IDE is lagging a lot but autoalign func almost done.

--- src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/commands/autoAlign.java
+++ src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/commands/autoAlign.java
@@ -4,12 +4,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import robot.src.main.java.org.frc1410.rebuilt2026.subsystems.Drivetrain;
 import robot.src.main.java.org.frc1410.rebuilt2026.util.Constants;
 import robot.src.main.java.org.frc1410.rebuilt2026.util.Tuning;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 
 public class autoAlign extends Command {
     private final Drivetrain drivetrain;
-    public autoAlign(Drivetrain dt){
+    private final double targetYaw;
+    public autoAlign(Drivetrain dt, double yaw){
         this.drivetrain = dt;
+        this.targetYaw = yaw;
     }
     
    
@@ -20,6 +23,11 @@ public class autoAlign extends Command {
     }
      @Override
     public void execute(){
+        this.drivetrain.drive(new ChassisSpeeds(
+            0, 
+            0, 
+            1.0 * targetYaw * Tuning.VISION_TURN_kP * Constants.SWERVE_DRIVE_MAX_ANGULAR_VELOCITY
+        ));
         //drivetrain.
         //1.0 * targetYaw * Tuning.VISION_TURN_kP * Constants.SWERVE_DRIVE_MAX_ANGULAR_VELOCITY;
     }
