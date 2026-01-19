0df189ac8c91be9529743a16765ec129dae31fd8
Calstar9000 <Calstar9000+github@gmail.com>
2026-01-19 16:30:03 -0700

    AUTO STUFF (DEF ORIGIONAL) HHEHEHEHEHHEHHEHEHEHEHEHHEHEHEHEHEHEHEHEHHEHEHEHEHEHE

--- src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Robot.java
+++ src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Robot.java
@@ -1,5 +1,7 @@
 package robot.src.main.java.org.frc1410.rebuilt2026;
 
+import framework.src.main.java.org.frc1410.framework.AutoSelector;
+
 // HELP
 
 import framework.src.main.java.org.frc1410.framework.PhaseDrivenRobot;
@@ -8,23 +10,88 @@ import framework.src.main.java.org.frc1410.framework.scheduler.task.TaskPersiste
 import framework.src.main.java.org.frc1410.framework.scheduler.task.lock.LockPriority;
 import robot.src.main.java.org.frc1410.rebuilt2026.commands.DriveLooped;
 import robot.src.main.java.org.frc1410.rebuilt2026.subsystems.Drivetrain;
+import robot.src.main.java.org.frc1410.rebuilt2026.util.NetworkTables;
+
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.DRIVER_CONTROLLER;
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.OPERATOR_CONTROLLER;
+import static robot.src.main.java.org.frc1410.rebuilt2026.util.Constants.*;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.networktables.StringSubscriber;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public final class Robot extends PhaseDrivenRobot {
-	public Robot() {}
 
-	private final Controller driverController = new Controller(this.scheduler, DRIVER_CONTROLLER, 0.1);
-	private final Controller operatorController = new Controller(this.scheduler, OPERATOR_CONTROLLER,  0.1);
-	private final Drivetrain drivetrain = subsystems.track(new Drivetrain(this.subsystems));
-	// private final Storage storage = new Storage();
 
-	// private final StorageToggleCommand intake = new StorageToggleCommand(storage, Storage.StorageStates.INTAKE);
-	// private final StorageToggleCommand neutral = new StorageToggleCommand(storage, Storage.StorageStates.NEUTRAL);
-	// private final StorageToggleCommand outtake = new StorageToggleCommand(storage, Storage.StorageStates.OUTTAKE);
+    private final Controller driverController = new Controller(this.scheduler, DRIVER_CONTROLLER, 0.1);
+    private final Controller operatorController = new Controller(this.scheduler, OPERATOR_CONTROLLER, 0.1);
+    private final Drivetrain drivetrain = subsystems.track(new Drivetrain(this.subsystems));
+
+	private final NetworkTableInstance nt = NetworkTableInstance.getDefault();
+	private final NetworkTable table = this.nt.getTable("Auto");
+
+
+	private final AutoSelector autoSelector = new AutoSelector()
+			.add("2", () -> new PathPlannerAuto("2 coral"));
+
+			 {
+				{
+		var profiles = new String[this.autoSelector.getProfiles().size()];
+		for (var i = 0; i < profiles.length; i++) {
+			profiles[i] = this.autoSelector.getProfiles().get(i).name();
+		}
+
+		var autoChoicesPub = NetworkTables.PublisherFactory(this.table, "Choices", profiles);
+		autoChoicesPub.accept(profiles);
+		}
+	}
+
+
+    public Robot() {
+		AutoBuilder.configure(
+			this.drivetrain::getEstimatedPosition,
+			this.drivetrain::resetPose,
+			this.drivetrain::getChassisSpeeds,
+			this.drivetrain::drive,
+			HOLONOMIC_AUTO_CONFIG,
+			ROBOT_CONFIG,
+			() -> {
+				var alliance = DriverStation.getAlliance();
+
+				if(alliance.isPresent()) {
+					return alliance.get() == DriverStation.Alliance.Red;
+				}
+				return false;
+			},
+			drivetrain
+		);
+
+    }
+
+	private final StringPublisher autoPublisher = NetworkTables.PublisherFactory(
+		this.table, 
+		"Profile",
+		this.autoSelector.getProfiles().isEmpty() ? "0" : this.autoSelector.getProfiles().
+			get(0)
+			.name()
+	);
+
+	private final StringSubscriber autoSubscriber = NetworkTables.SubscriberFactory(this.table, this.autoPublisher.getTopic());
+
 
 	@Override
 	public void autonomousSequence() {
+		NetworkTables.SetPersistence(this.autoPublisher.getTopic(), true);
+			String autoProfile = this.autoSubscriber.get();
+			var autoCommand = this.autoSelector.select(autoProfile);
+
+			this.scheduler.scheduleAutoCommand(autoCommand);
+
 	}
 
 	@Override
