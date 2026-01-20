9450c25516dd8f26d4fd2a08ea932967007c4626
Greek Question Mark <galaxyisfake@gmail.com>
2026-01-20 11:27:06 -0700

    Arraylists are annoying

--- src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Vision/Cam.java
+++ src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Vision/Cam.java
@@ -2,6 +2,7 @@ package robot.src.main.java.org.frc1410.rebuilt2026.Vision;
 
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.Constants.APRIL_TAG_FIELD_LAYOUT;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,34 +21,25 @@ public class Cam implements TickedSubsystem{
     private final PhotonCamera cam;
     private final Transform3d offset;
     private final PhotonPoseEstimator poseEst;
-    private final List<PhotonPipelineResult> results;
-        //
-
-    public Cam(String name, Transform3d offset){
-        this.cam = new PhotonCamera(name);
-        this.offset = offset;
-         poseEst = new PhotonPoseEstimator(
-                APRIL_TAG_FIELD_LAYOUT,
-                PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-                this.offset
-        );
-    }
-    @Override
-    public void periodic() {
-        // RANDOM BS GOOOOOOOOOOOOOO
-        results = cam.getAllUnreadResults();
+    private List<PhotonPipelineResult> results = new ArrayList<>();
+            //
+    
+        public Cam(String name, Transform3d offset){
+            this.cam = new PhotonCamera(name);
+            this.offset = offset;
+             poseEst = new PhotonPoseEstimator(
+                    APRIL_TAG_FIELD_LAYOUT,
+                    PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+                    this.offset
+            );
+        }
+        @Override
+        public void periodic() {
+            // RANDOM BS GOOOOOOOOOOOOOO
+            results = cam.getAllUnreadResults();
         
     }
     public List<PhotonPipelineResult> getUnreadResults(){
         return results;
-    }
-    public PhotonPipelineResult getBestResult(){
-        return ((Cam) results).getBestResult();
-    }
-    public boolean hasTargets{
-        return ((PhotonPipelineResult) results).hasTargets();
-    }
-    public PoseStrategy returnPoseStrategy{
-        return poseEst.getPrimaryStrategy();
     }
 }
