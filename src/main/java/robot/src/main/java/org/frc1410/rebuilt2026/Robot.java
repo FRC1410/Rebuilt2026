a1f0e658c03d98d131eaeae2c8bcb802660b0f78
Greek Question Mark <galaxyisfake@gmail.com>
2026-01-21 11:05:57 -0700

    fixes so that robot actually drives lol

--- src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Robot.java
+++ src/main/java/robot/src/main/java/org/frc1410/rebuilt2026/Robot.java
@@ -12,8 +12,13 @@ import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.DRIVER_CONTRO
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.OPERATOR_CONTROLLER;
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.Tuning.EoC1_OFFSET;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import static robot.src.main.java.org.frc1410.rebuilt2026.util.Constants.HOLONOMIC_AUTO_CONFIG;
+import static robot.src.main.java.org.frc1410.rebuilt2026.util.Constants.ROBOT_CONFIG;
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.CAM_NAME1;
 import static robot.src.main.java.org.frc1410.rebuilt2026.util.IDs.CAM_NAME2;
 
@@ -22,12 +27,31 @@ import framework.src.main.java.org.frc1410.framework.scheduler.task.lock.LockPri
 import robot.src.main.java.org.frc1410.rebuilt2026.Vision.*;
 
 public final class Robot extends PhaseDrivenRobot {
-	public Robot() {}
 	Vision kv = new Vision(CAM_NAME1, EoC1_OFFSET);
 	private final Controller driverController = new Controller(this.scheduler, DRIVER_CONTROLLER, 0.1);
 	private final Controller operatorController = new Controller(this.scheduler, OPERATOR_CONTROLLER,  0.1);
 	private final Drivetrain drivetrain = subsystems.track(new Drivetrain(this.subsystems));
 
+	public Robot() {
+		AutoBuilder.configure(
+			this.drivetrain::getEstimatedPosition,
+			this.drivetrain::resetPose,
+			this.drivetrain::getChassisSpeeds,
+			this.drivetrain::drive,
+			HOLONOMIC_AUTO_CONFIG,
+			ROBOT_CONFIG,
+			() -> {
+				var alliance = DriverStation.getAlliance();
+
+				if(alliance.isPresent()) {
+					return alliance.get() == DriverStation.Alliance.Red;
+				}
+				return false;
+			},
+			drivetrain
+		);
+
+    }
 	@Override
 	public void autonomousSequence() {
 	}
